6beb6323eec9e95647469128213ae6588ebb17cd
Mark Winters <nulimitz360@gmal.com>
2025-03-29 08:00:16 -0600

    first commit

---
+++ src/app/(admin)/layout.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function Adminlayout() {
+  return <div>Adminlayout</div>;
+}
---
+++ src/app/(user)/dreams/page.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function DreamsPage() {
+  return <div>DreamsPage</div>;
+}
+
+export default DreamsPage;
